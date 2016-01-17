996138b53ac3980d6485f706b5ef31c29acff5b4
Eric Bower <neurosnap@gmail.com>
2016-01-17 15:05:26 -0500

    abstracting response codes

--- server.c
+++ server.c
@@ -37,20 +37,55 @@ int httpListen(int port, int max_connections) {
     return http_socket;
 }
 
+void httpHeaderDate(char *date, struct tm *time) {
+    //Date: Sun, 17 Jan 2016 19:19:37 GMT\n
+    char *day[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    char *month[12] = {
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+
+    sprintf(
+        date,
+        "Date: %s, %2d %s %d %2d:%2d:%2d GMT\n",
+        day[time->tm_wday],
+        time->tm_mday,
+        month[time->tm_mon],
+        time->tm_year + 1900,
+        time->tm_hour,
+        time->tm_hour,
+        time->tm_sec
+    );
+}
+
+void httpStatus(int sock, int status) {
+    char http_proto[16];
+    char http_status[15];
+    send(sock, sprintf(http_proto, "HTTP/1.1 %d OK\n", status), 16, 0);
+    send(sock, "Status: %d OK\n", 15, 0);
+}
+
 int main(int argc, char *argv[]) {
     char *port = argv[1];
+    if (port == NULL) {
+        printf("First argument needs to be the port, e.g.: 1337\n");
+        return 1;
+    }
+
     struct sockaddr_in client;
+    time_t t = time(NULL);
     unsigned int client_len;
     int client_socket;
     int max_connections = 1000;
+
     int http_socket = httpListen(atoi(port), max_connections);
-    time_t t = time(NULL);
 
     int bufSize = 1024 * 6;
     char *buffer = malloc(bufSize);
     for (;;) {
-        char date[33] = "Date: ";
-        sprintf(date, "%s\n", asctime(gmtime(&t)));
+        struct tm tm = *gmtime(&t);
+        char date[42];
+        httpHeaderDate(date, &tm);
 
         client_len = sizeof(client);
         client_socket = accept(http_socket, (struct sockaddr *) &client, &client_len);
@@ -64,13 +99,14 @@ int main(int argc, char *argv[]) {
 
         //printf("Message from client:\n%s\n", buffer);
 
-        send(client_socket, "HTTP/1.1 200 OK\n", 16, 0);
+        httpStatus(client_socket, 200);
         send(client_socket, "Server: chttp\n", 14, 0);
-        send(client_socket, "Content-Length: 3\n", 18, 0);
-        send(client_socket, "Status: 200 OK\n", 15, 0);
         send(client_socket, "Connection: close\n", 18, 0);
-        //send(client_socket, date, 33, 0);
-        send(client_socket, "Content-Type: text/html\n\n", 25, 0);
+        send(client_socket, "Allow: GET\n", 11, 0);
+        send(client_socket, "Content-Type: text/html\n", 24, 0);
+        send(client_socket, "Content-Length: 3\n", 18, 0);
+        send(client_socket, date, sizeof(date), 0);
+        send(client_socket, "\n\n", 2, 0);
 
         send(client_socket, "Hi!", 3, 0);
         close(client_socket);
